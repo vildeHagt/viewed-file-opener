a2089e4d887a4a0ea5a4af881da65a4642664741
Vilde Hagtvedt <54098897+vildeHagt@users.noreply.github.com>
2025-06-26 20:38:30 +0200

    fix

--- scripts/button.js
+++ scripts/button.js
@@ -1,18 +1,15 @@
 const viewedFile = document.getElementById("manifest.json-viewed-file");
 
-console.log("ActionBar found");
-
 const buttonDiv = document.getElementsByClassName(
   "flex-grow-0 flex-shrink-0 pr-review-tools"
 )[0];
 const button = document.createElement("button");
 button.type = "button";
-button.style.backgroundColor = "#212830";
-button.style.borderColor = "#3d444d";
-button.textContent = "Open Viewed files";
+button.className = "open-viewed-files-btn";
+button.textContent = "Open viewed files";
 
 button.addEventListener("click", () => {
-  const prText = openViewedFiles();
+  openViewedFiles();
 });
 
-buttonDiv.appendChild(button);
+buttonDiv.prepend(button);
